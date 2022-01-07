7f94cda46a5f505bae5003d8264d4d6354ab2cab
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-07 17:48:07 -0500

    create reactionSchema for the user  in models/Thought.js and add reactionId & reactionBody to it

---
+++ models/Thought.js
@@ -0,0 +1,14 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+});
