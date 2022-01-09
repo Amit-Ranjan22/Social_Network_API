c1b89d18fcbfe8955d3b4ab94bb5dc634a2b20c3
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-09 14:03:43 -0500

    write route controller to delete a user by id in controllers/user-controllers.js

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -65,4 +65,30 @@ const userController = {
         res.status(400).json(err);
       });
   },
+  deleteUser({ params }, res) {
+    User.findByIdAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        dbUserData.thoughts.forEach((thought) => {
+          Thoughts.findOneAndDelete({ _id: thought })
+            .then((dbThoughtData) => {
+              if (!dbThoughtData) {
+                res
+                  .status(500)
+                  .json({ message: "Cannot delete user.There was an error!" });
+                return;
+              }
+
+              res.json(dbUserData);
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(400).json(err);
+            });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 };
