4c982ab9407bfc5eb921f60f20543ead3d8f2e18
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-09 13:53:02 -0500

    write route controller to create a user in controllers/user-controllers.js

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -37,4 +37,14 @@ const userController = {
         res.status(400).json(err);
       });
   },
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => {
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 };
