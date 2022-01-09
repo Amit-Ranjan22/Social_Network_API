5d7e76ed1814e4009412623c3d6fa1735ffb48a0
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-09 13:50:44 -0500

    write route controller to get a user by its id in controllers/user-controllers.js

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -18,4 +18,23 @@ const userController = {
         res.status(400).json(err);
       });
   },
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found for this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 };
