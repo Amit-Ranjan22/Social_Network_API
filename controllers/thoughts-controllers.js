7a96bb2aa62788d2585bf26b4d0f79e945cb2369
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-09 14:37:03 -0500

    write thoughts controller for route to get a single thought by id in controllers/thoughts-controllers.js

--- controllers/thoughts-controllers.js
+++ controllers/thoughts-controllers.js
@@ -16,4 +16,23 @@ thoughtsControllers = {
         res.status(400).json(err);
       });
   },
+  getThoughtsById({ params }, res) {
+    Thoughts.findOne({ _id: params.id })
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbThoughtsData) => {
+        if (!dbThoughtsData) {
+          res.status(404).json({ message: "No thought found for this id" });
+          return;
+        }
+        res.json(dbThoughtsData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 };
