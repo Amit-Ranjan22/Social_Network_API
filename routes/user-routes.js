bbdd864c671e0333969e317e060d75b5abf1a795
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-09 17:01:40 -0500

    create post route to add friend & delete route to delete friend in routes/user-routes.js

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -13,3 +13,5 @@ const {
 router.route("/").get(getAllUsers).post(createUser);
 
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
