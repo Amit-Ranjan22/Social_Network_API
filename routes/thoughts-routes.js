b7f4e0e2e0fd6972f67eaa47f5c4213a6308e1c7
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-10 12:39:18 -0500

    require all the methods created in thoughts-controllers.js by using destructuring in routes/user-routes.js

--- routes/thoughts-routes.js
+++ routes/thoughts-routes.js
@@ -1,1 +1,11 @@
 const router = require('express').Router();
+
+const {
+  getThoughts,
+  getThoughtsById,
+  createThoughts,
+  updateThoughts,
+  deleteThoughts,
+  createReaction,
+  removeReaction,
+} = require("../controllers/thoughts-controllers");
