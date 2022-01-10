b8638d999a708c6f2022e204c22b7c45609f717d
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-10 12:48:26 -0500

    create route to remove reaction and export router at the end in routes/thoughts-routes.js

--- routes/thoughts-routes.js
+++ routes/thoughts-routes.js
@@ -19,3 +19,7 @@ router
   .delete(deleteThoughts);
 
 router.route("/:thoughtId/reactions").post(createReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
